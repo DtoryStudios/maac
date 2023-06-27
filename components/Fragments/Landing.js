b00d0cdced049ddda6cabfc72660588fc2b4f6a9
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-27 09:54:00 +0530

    response sheet

--- components/Fragments/Landing.js
+++ components/Fragments/Landing.js
@@ -1,75 +1,148 @@
-import React, { useState } from "react";
+import courses from "@/static/courses";
+import React, { useEffect, useState } from "react";
 
 function Landing() {
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
+    phone: "",
+    email: "",
+    course: "",
   });
+  useEffect(() => {
+    loading
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "unset");
+  }, [loading]);
   return (
-    <div className="hero-image min-h-screen w-full lg:flex items-center py-28 lg:py-32 px-6 lg:px-28 lg:space-x-5 bg-red-50">
-      <div className="lg:w-1/2">
-        <h1 className="text-5xl lg:text-7xl font-bold leading-[1.4] lg:leading-[1.2] text-white">
-          Build your career with the best.
-        </h1>
-        <p className="mt-6 leading-6 lg:leading-8 text-white lg:text-sm text-xs">
-          Allow your imagination flame to come to life with energy. Our
-          animation institute in Durgapur will help you to enhance your talents
-          and prepare you for a successful career and higher opportunities
-        </p>
-        <button className="text-sm px-7 py-3 lg:py-4 bg-[#EAC435] hover:bg-[#c2a12d] text-black mt-5 font-medium rounded font-poppins transition-all">
-          <span>Start Now - Admission Open 523</span>
-        </button>
-      </div>
-      <div className="lg:w-1/2 flex items-center justify-center mt-16 lg:mt-0">
-        <div className="w-full max-w-[500px] p-6 lg:p-8 bg-white/5 backdrop-blur-2xl text-white">
-          <h1 className="font-semibold text-lg font-poppins text-center">
-            Career Counselling
+    <>
+      <div className="hero-image min-h-screen w-full lg:flex items-center py-28 lg:py-32 px-6 lg:px-28 lg:space-x-5 bg-red-50">
+        <div className="lg:w-1/2">
+          <h1 className="text-5xl lg:text-7xl font-bold leading-[1.4] lg:leading-[1.2] text-white">
+            Build your career with the best.
           </h1>
-          <form action="" className="grid grid-cols-2 gap-5 mt-8">
-            <input
-              type="text"
-              placeholder="Your full name"
-              value={formData.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
-              className="bg-black/50 placeholder:text-white/50 px-5 py-5 col-span-2 rounded"
-              name=""
-              id=""
-            />
-            <input
-              type="tel"
-              value={formData.phone}
-              onChange={(e) =>
-                setFormData({ ...formData, phone: e.target.value })
-              }
-              placeholder="Your phone number"
-              className="bg-black/50 col-span-2 lg:col-span-1 placeholder:text-white/50 px-5 py-5 rounded"
-              name=""
-              id=""
-            />
-            <input
-              type="text"
-              placeholder="Your email address"
-              className="bg-black/50 col-span-2 lg:col-span-1 placeholder:text-white/50 px-5 py-5 rounded"
-              name=""
-              id=""
-            />
-            <input
-              type="text"
-              placeholder="Select your courses"
-              className="bg-black/50 placeholder:text-white/50 col-span-2 px-5 py-5 rounded"
-              name=""
-              id=""
-            />
-            <div className="col-span-2">
-              <button className="mt-5 w-full bg-white text-black text-center py-4 rounded">
-                Submit
-              </button>
-            </div>
-          </form>
+          <p className="mt-6 leading-6 lg:leading-8 text-white lg:text-sm text-xs">
+            Allow your imagination flame to come to life with energy. Our
+            animation institute in Durgapur will help you to enhance your
+            talents and prepare you for a successful career and higher
+            opportunities
+          </p>
+          <button className="text-sm px-7 py-3 lg:py-4 bg-[#EAC435] hover:bg-[#c2a12d] text-black mt-5 font-medium rounded font-poppins transition-all">
+            <span>Start Now - Admission Open 523</span>
+          </button>
+        </div>
+        <div className="lg:w-1/2 flex items-center justify-center mt-16 lg:mt-0">
+          <div className="w-full max-w-[500px] p-6 lg:p-8 bg-white/5 backdrop-blur-2xl text-white">
+            <h1 className="font-semibold text-lg font-poppins text-center">
+              Career Counselling
+            </h1>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                setLoading(true);
+                const res = await fetch(
+                  "https://script.google.com/macros/s/AKfycbx6nI4Z0nJFH5SbLreVfQBtBLUJMfeObyUEA5CQqlvb-dQEYiWE-05RoC-B4d8GJKfW/exec",
+                  {
+                    method: "POST",
+                    body: JSON.stringify(formData),
+                  }
+                );
+                const data = await res.json();
+                if (data.status == true) {
+                  setLoading(false);
+                  alert("Form submitted successfully");
+                  setFormData({
+                    name: "",
+                    phone: "",
+                    email: "",
+                    course: "",
+                  });
+                }
+              }}
+              className="grid grid-cols-2 gap-5 mt-8"
+            >
+              <input
+                type="text"
+                placeholder="Your full name"
+                required
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                className="bg-black/50 placeholder:text-white/50 px-5 py-5 col-span-2 rounded"
+                name=""
+                id=""
+              />
+              <input
+                type="tel"
+                required
+                value={formData.phone}
+                onChange={(e) =>
+                  setFormData({ ...formData, phone: e.target.value })
+                }
+                placeholder="Your phone number"
+                className="bg-black/50 col-span-2 lg:col-span-1 placeholder:text-white/50 px-5 py-5 rounded"
+                name=""
+                id=""
+              />
+              <input
+                type="email"
+                required
+                placeholder="Your email address"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                className="bg-black/50 col-span-2 lg:col-span-1 placeholder:text-white/50 px-5 py-5 rounded"
+                name=""
+                id=""
+              />
+
+              <input
+                type="text"
+                list="browsers"
+                required
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    course: e.target.value,
+                  });
+                }}
+                placeholder="Select your courses"
+                className="bg-black/50 placeholder:text-white/50 col-span-2 px-5 py-5 rounded transition-all"
+                name=""
+                id=""
+              />
+
+              <datalist id="browsers">
+                {courses.map((course, index) => {
+                  return <option key={index} value={course.name} />;
+                })}
+              </datalist>
+              <div className="col-span-2">
+                <button
+                  type="submit"
+                  disabled={
+                    formData.name === "" ||
+                    formData.phone === "" ||
+                    formData.email === "" ||
+                    formData.course === ""
+                  }
+                  className="mt-5 w-full disabled:opacity-50 bg-white text-black text-center py-4 rounded"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      {loading && (
+        <div className="fixed inset-0 h-full w-full z-30 bg-black/50 backdrop-blur-sm flex items-center justify-center">
+          <div className="w-32 h-32 border border-w border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+    </>
   );
 }
 
