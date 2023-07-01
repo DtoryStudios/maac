18d28eab8190584e55a484c9eb392df4ce0a8b29
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-01 14:44:58 +0530

    carousel

--- static/courses.js
+++ static/courses.js
@@ -7,41 +7,49 @@ const courses = [
   {
     name: "Professional Program in 3d Animation Xpert",
     duration: "96 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/pp3ax.svg",
   },
   {
     name: "Explore the world of visual effects",
     duration: "96 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/ewve.svg",
   },
   {
     name: "Motion Graphics PRO",
     duration: "96 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/mgp.svg",
   },
   {
     name: "Visual Effects NEXT",
     duration: "96 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/ven.svg",
   },
   {
     name: "3D Animation PRO",
     duration: "126 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/3ap.svg",
   },
   {
     name: "Archi Viz Next",
     duration: "140 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/avn.svg",
   },
   {
     name: "Multimedia & Digital Design",
     duration: "126 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/mdd.svg",
   },
   {
     name: "Graphics Design Pro",
     duration: "126 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/gdp.svg",
   },
   {
@@ -52,11 +60,13 @@ const courses = [
   {
     name: "Web Design Pro",
     duration: "126 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/wdp.svg",
   },
   {
     name: "Compositing Pro",
     duration: "126 hours",
+    bg: `imageContainer h-full w-full bg-emerald-50  object-cover object-center bg-[url(/courses/pp3av.svg)]]`,
     image: "/courses/cp.svg",
   },
 ];
