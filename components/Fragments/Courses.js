d2c92f85744e7cff7a061ef9fa418b80a539260e
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-01 14:45:02 +0530

    cp

--- components/Fragments/Courses.js
+++ components/Fragments/Courses.js
@@ -11,14 +11,19 @@ const Carousel = dynamic(() => import("react-spring-3d-carousel"), {
 });
 
 function Courses() {
+  const returnBgUrl = (url) => {
+    return `url(${url})`;
+  };
   const slides = courses.map((course) => {
     return {
       key: uuidv4(),
       content: (
-        <div className="h-[550px] w-[80vw] relative">
-          <img src={course.image} alt={course.title} />
-          <div className="absolute bottom-0 p-7 z-10">
-            <h2 className="text-white text-3xl font-bold">{course.name}</h2>
+        <div className="h-[500px] lg:h-[550px] lg:w-[900px] w-screen">
+          <div className={`${course.bg}`}></div>
+          <div className="absolute left-0 bottom-10 px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {course.name}
+            </h2>
             <p className="text-base mt-5 text-white flex items-center space-x-2">
               <iconify-icon icon="mdi:clock"></iconify-icon>
               <span> Duration: {course.duration}</span>
@@ -31,6 +36,249 @@ function Courses() {
       link: course.link,
     };
   });
+
+  const slidesCopy = [
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/pp3av.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[0].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[0].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[0].title,
+      description: courses[0].description,
+      link: courses[0].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/pp3ax.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[1].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[1].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[1].title,
+      description: courses[1].description,
+      link: courses[1].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/ewve.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[2].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[2].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[2].title,
+      description: courses[2].description,
+      link: courses[2].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/mgp.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[3].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[3].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[3].title,
+      description: courses[3].description,
+      link: courses[3].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/ven.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[4].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[4].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[4].title,
+      description: courses[4].description,
+      link: courses[4].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/3ap.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[5].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[5].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[5].title,
+      description: courses[5].description,
+      link: courses[5].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/avn.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[6].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[6].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[6].title,
+      description: courses[6].description,
+      link: courses[6].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/mdd.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[7].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[7].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[7].title,
+      description: courses[7].description,
+      link: courses[7].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/gdp.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[8].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[8].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[8].title,
+      description: courses[8].description,
+      link: courses[8].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/mgan.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[9].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[9].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[9].title,
+      description: courses[9].description,
+      link: courses[9].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/wdp.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[10].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[10].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[10].title,
+      description: courses[10].description,
+      link: courses[10].link,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <div className="h-[500px] lg:h-[550px] lg:w-[1000px] w-screen">
+          <div className="h-full w-full bg-red-50 bg-[url('/courses/cp.svg')]"></div>
+          <div className="absolute left-0 bottom-10 px-6 lg:px-16 z-10">
+            <h2 className="text-white text-xl lg:text-3xl font-bold">
+              {courses[11].name}
+            </h2>
+            <p className="text-base mt-5 text-white flex items-center space-x-2">
+              <iconify-icon icon="mdi:clock"></iconify-icon>
+              <span> Duration: {courses[11].duration}</span>
+            </p>
+          </div>
+        </div>
+      ),
+      title: courses[11].title,
+      description: courses[11].description,
+      link: courses[11].link,
+    },
+  ];
   const [state, setState] = useState({
     goToSlide: 0,
     offsetRadius: 2,
@@ -52,7 +300,7 @@ function Courses() {
         <div className="mt-36">
           <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
             <Carousel
-              slides={slides}
+              slides={slidesCopy}
               goToSlide={state.goToSlide}
               offsetRadius={state.offsetRadius}
               animationConfig={state.config}
--- components/Search.js
+++ components/Search.js
@@ -39,58 +39,60 @@ function Search({ open, setOpen }) {
   return (
     <>
       {open && (
-        <div className="fixed inset-0 h-full w-full backdrop-blur bg-black/10 overflow-auto">
-          <div className="p-6 lg:p-32">
-            <div className="h-14 border border-white/20 lg:w-[60%] bg-[#212125] rounded-md flex items-center px-4 shadow-2xl shadow-white/50 lg:mx-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                class="w-5 h-5 text-white/50"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        <>
+          <div className="fixed inset-0 h-full w-full backdrop-blur bg-black/10 overflow-auto">
+            <div className="p-6 lg:p-32">
+              <div className="h-14 border border-white/20 lg:w-[60%] bg-[#212125] rounded-md flex items-center px-4 shadow-2xl shadow-white/50 lg:mx-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  class="w-5 h-5 text-white/50"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+                <input
+                  ref={input}
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  type="text"
+                  placeholder="Search for a course"
+                  className="bg-transparent outline-none text-white w-full placeholder:text-white/60 ml-3"
+                  name=""
+                  id=""
                 />
-              </svg>
-              <input
-                ref={input}
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                type="text"
-                placeholder="Search for a course"
-                className="bg-transparent outline-none text-white w-full placeholder:text-white/60 ml-3"
-                name=""
-                id=""
-              />
-            </div>
-            <div className="grid grid-cols-1 gap-3 mt-5 place-content-center place-items-center">
-              {results.map((course, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="lg:flex overflow-hidden items-center lg:h-32 lg:w-[60%] bg-black/20  hover:bg-black/30 rounded-md transition-all"
-                  >
-                    <img
-                      src={course.image}
-                      className="lg:h-32 lg:w-56 w-full object-cover"
-                      alt=""
-                    />
-                    <div className="ml-3 p-3">
-                      <h1 className="text-white font-bold text-2xl">
-                        {course.name}
-                      </h1>
-                      <p className="text-white text-xs mt-2 font-light flex items-center">
-                        <iconify-icon icon="mdi:clock"></iconify-icon>
-                        <span className="ml-2">{course.duration}</span>
-                      </p>
+              </div>
+              <div className="grid grid-cols-1 gap-3 mt-5 place-content-center place-items-center">
+                {results.map((course, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="lg:flex overflow-hidden items-center lg:h-32 lg:w-[60%] bg-black/20  hover:bg-black/30 rounded-md transition-all"
+                    >
+                      <img
+                        src={course.image}
+                        className="lg:h-32 lg:w-56 w-full object-cover"
+                        alt=""
+                      />
+                      <div className="ml-3 p-3">
+                        <h1 className="text-white font-bold text-2xl">
+                          {course.name}
+                        </h1>
+                        <p className="text-white text-xs mt-2 font-light flex items-center">
+                          <iconify-icon icon="mdi:clock"></iconify-icon>
+                          <span className="ml-2">{course.duration}</span>
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
           <button
@@ -112,7 +114,7 @@ function Search({ open, setOpen }) {
               />
             </svg>
           </button>
-        </div>
+        </>
       )}
     </>
   );
